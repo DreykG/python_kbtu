c5b0a79fc6becb785b652be297f3db4efb7ed998
Vladislav Lizko <vladlzk26@gmail.com>
2025-01-18 15:39:24 +0500

    Python Comments

--- lab1.py
+++ lab1.py
@@ -1,22 +1,24 @@
-#Indentation refers to the spaces at the beginning of a code line.
-#Where in other programming languages the indentation in code is for readability only, 
-#the indentation in Python is very important.
-#Python uses indentation to indicate a block of code.
+#Comments can be used to explain Python code.
+#Comments can be used to make the code more readable.
+#Comments can be used to prevent execution when testing code.
+#Comments starts with a #, and Python will ignore them:
 
-if 5 > 2:
-  print("Five is greater than two!")
-    
-#Python will give you an error if you skip the indentation:
-if 5 > 2:
-print("Five is greater than two!")
+#This is a comment
+print("Hello, World!")
 
-#The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
-if 5 > 2:
- print("Five is greater than two!") 
-if 5 > 2:
-        print("Five is greater than two!") 
+print("Hello, World!") #This is a comment
 
-#You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
-if 5 > 2:
- print("Five is greater than two!")
-        print("Five is greater than two!")
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
