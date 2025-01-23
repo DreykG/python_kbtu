0359f0a078aac13d5fd7d36dee9f55b24204e785
Vladislav Lizko <vladlzk26@gmail.com>
2025-01-23 12:40:27 +0500

    check

--- lab1.py
+++ lab1.py
@@ -1,2 +1,14 @@
-Все 13 тем хранятся в Истории(Отчетах об изменениях). 
-Там каждый commit содержит примеры и описание темы, соответствующему названию commit
+from random import *
+
+boys = ['Vlad','Egor','Isa','Anatoliy','Timur']
+girls = ['Marina','Katya','Elena','Daria','Alina']
+a = "Mustafa"
+boys.append(a)
+for i in range(0,len(boys)):
+    if "a" in boys[i]:
+        boys.remove(boys[i])
+        i = 1
+        
+        
+for j in boys:
+    print(j)
