8371b1fa1620208d98c49d6dc4ef588f53c28e46
Vladislav Lizko <vladlzk26@gmail.com>
2025-01-29 20:56:19 +0500

    task3

--- lab3.py
+++ lab3.py
@@ -1,5 +1,23 @@
-def transfer(degree):
-    return (float(5/9)*(int(degree) - 32))
+def count(legs,heads):
+    rab = (legs - 2*heads)/2
+    chick = heads - rab
+    print("Number of rabbit =", rab)
+    print("Number of chickens =", chick)
+    
+legs = int(input("input legs:\n"))
+heads = int(input("input heads:\n"))
+count(legs,heads)
 
-f = int(input("How much degree by Faranheit?"))
-print(round(transfer(f),1), "by Celcius")
+
+
+# legs = 94
+# head = 35
+
+# 4*r + 2*c = 94
+# r+c=35
+# c = 35-r
+# 4r +70-2r=94
+# 2r=24
+# r=12
+# c=23
+# 4*12 + 2*23 = 48+46
