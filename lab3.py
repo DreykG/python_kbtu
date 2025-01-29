3dfcb0bb1d4b3fe1f9bf6933305e714e20253e82
Vladislav Lizko <vladlzk26@gmail.com>
2025-01-29 21:15:02 +0500

    task8

--- lab3.py
+++ lab3.py
@@ -1,13 +1,13 @@
-def check(numbers):
+def spy_game(numbers):
     a = False
-    for i in range(0,len(numbers)-1):
-        if int(numbers[i]) == 3 and int(numbers[i+1]) == 3:
+    for i in range(0,len(numbers)-2):
+        if int(numbers[i]) == 0 and int(numbers[i+1]) == 0 and int(numbers[i+2]) == 7:
             a = True
             break
     return a
             
 list = input("Input numbers:\n").split()
-if check(list):
+if spy_game(list):
     print("True")
 else:
     print("False")
