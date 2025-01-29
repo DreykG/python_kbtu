64ce3f8b248fcc9a0d57a7383e2db340abd2ff83
Vladislav Lizko <vladlzk26@gmail.com>
2025-01-29 21:24:59 +0500

    task9

--- lab3.py
+++ lab3.py
@@ -1,13 +1,6 @@
-def spy_game(numbers):
-    a = False
-    for i in range(0,len(numbers)-2):
-        if int(numbers[i]) == 0 and int(numbers[i+1]) == 0 and int(numbers[i+2]) == 7:
-            a = True
-            break
-    return a
-            
-list = input("Input numbers:\n").split()
-if spy_game(list):
-    print("True")
-else:
-    print("False")
+def volume(r):
+    v=round(float((4*r**3)/3),1)
+    print("Volume with 'п' = " + str(v) + "п")
+
+r = float(input("Input radius:\n"))
+volume(r)
