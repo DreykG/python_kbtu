6687fa3d29eca39c203b0029a7f44892c483eb2e
Vladislav Lizko <vladlzk26@gmail.com>
2025-01-29 21:52:40 +0500

    task10

--- lab3.py
+++ lab3.py
@@ -1,6 +1,14 @@
-def volume(r):
-    v=round(float((4*r**3)/3),1)
-    print("Volume with 'п' = " + str(v) + "п")
-
-r = float(input("Input radius:\n"))
-volume(r)
+def uniq(list):
+    i=0
+    while i <len(list):
+        j = i+1
+        while j<len(list):
+            if list[i] == list[j]:
+                del list[j]
+            else:
+                j += 1
+        i += 1
+    print(list)    
+    
+list = list(map(int, input("Input your numbers:\n").split()))
+uniq(list)
