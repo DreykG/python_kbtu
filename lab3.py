285e159fcafada500ce56553a80120b003dbfca0
Vladislav Lizko <vladlzk26@gmail.com>
2025-01-30 11:28:21 +0500

    task13

--- lab3.py
+++ lab3.py
@@ -1,6 +1,26 @@
-def histo(list):
-    for i in list:
-        print(i*"*")
+from random import randint
+def fijma(x,name):
+    guess = 0
+    while True:
+        a = int(input("Take a guess.\n"))
+        if a < x:
+            print("Your guess is too low.")
+            guess+=1
+        if a > x:
+            print("Your guess is too high.")
+            guess+=1
+        if a==x:
+            guess+=1
+            print(f"Good job, {name}! You guessed my number in {guess} guesses!")
+            break
+            
+    
+    
         
-list = list(map(int, input("Input your numbers:\n").split()))
-histo(list)
+name = input("Hello! What is your name?\n")
+print(f"Well, {name}, I am thinking of a number between 1 and 20.")
+x = randint(1,20)
+fijma(x,name)
+
+
+
