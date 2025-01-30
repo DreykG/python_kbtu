11e99a031edd9dfdab58383919f7ef2e085fcc01
Vladislav Lizko <vladlzk26@gmail.com>
2025-01-30 11:11:20 +0500

    task11

--- lab3.py
+++ lab3.py
@@ -1,14 +1,11 @@
-def uniq(list):
-    i=0
-    while i <len(list):
-        j = i+1
-        while j<len(list):
-            if list[i] == list[j]:
-                del list[j]
-            else:
-                j += 1
-        i += 1
-    print(list)    
-    
-list = list(map(int, input("Input your numbers:\n").split()))
-uniq(list)
+def palidrome(string):
+    if string == string[::-1]:
+        return True
+    else:
+        return False
+
+s = input("Input your word\n")
+if palidrome(s):
+    print("Yes")
+else:
+    print("No")
